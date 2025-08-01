605fe35f91584dc98c83187f47d1f7aea631d9e8
ke-kaa <amsalukaku122@gmail.com>
2025-08-01 15:29:41 +0300

    Commit at 15:29:41

---
+++ backend/src/models/Upgrade.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from 'mongoose';
+
+const UpgradeSchema = new Schema({
+    icon: { type: String },
+    name: { type: String, required: true },
+    currentLevel: { 
+        type: Number, 
+        required: true, 
+        min: 0
+    },
+    nextLevel: { 
+        type: Number, 
+        required: true, 
+        min: 1
+    },
+    status: {
+        type: String, 
+        enum: ['Pending', 'In Progress', 'Completed', 'Cancelled'],
+        default: 'Pending'
+    },
+    upgradeStartTime: { type: Date },
+    upgradeEndTime: { type: Date },
+    upgradeCost: { type: Number, default: 0 },
+    upgradeTime: { type: Number, default: 0 }, // in seconds
+    account: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
+    item: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'itemType'
+    },
+    itemType: {
+        type: String, 
+        required: true,
+        enum: ['Building', 'Hero', 'Troop', 'Spell', 'Siege', 'Pet']
+    }
+}, { timestamps: true });
+
+export default mongoose.model("Upgrade", UpgradeSchema);
