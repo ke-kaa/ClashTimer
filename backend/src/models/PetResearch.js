5ee1708d066e9d3a3a4699bef20cfe5d6c8cd6bd
ke-kaa <amsalukaku122@gmail.com>
2025-08-01 10:05:33 +0300

    Commit at 10:05:33

---
+++ backend/src/models/PetResearch.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from 'mongoose';
+
+const PetResearchSchema = new Schema({
+    icon: { type: String },
+    name: { type: String, required: true },
+    petType: { 
+        type: String, 
+        enum: ['L.A.S.S.I', 'Electro Owl', 'Mighty Yak', 'Unicorn', 'Phoenix', 'Poison Lizard', 'Diggy', 'Frosty', 'Spirit Fox', 'Angry Jelly'],
+        required: true 
+    },
+    currentLevel: { 
+        type: Number, 
+        required: true, 
+        min: 0,
+        validate: {
+            validator: function(v) {
+                return v <= this.maxLevel;
+            },
+            message: 'Current level cannot exceed max level'
+        }
+    },
+    maxLevel: { type: Number, required: true, min: 1 },
+    status: {
+        type: String, 
+        enum: ['Idle', 'Researching'],
+        default: 'Idle'
+    },
+    researchStartTime: { type: Date },
+    researchEndTime: { type: Date },
+    researchCost: { type: Number, default: 0 },
+    researchTime: { type: Number, default: 0 }, // in seconds
+    account: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
+    pet: {
+        type: Schema.Types.ObjectId,
+        ref: 'Pet'
+    }
+}, { timestamps: true });
+
+export default mongoose.model("PetResearch", PetResearchSchema);
