0360c846bb55d2443fffcf0db271c5efbf9ab91f
ke-kaa <amsalukaku122@gmail.com>
2025-08-09 16:55:59 +0300

    create centralized route handler

---
+++ backend/src/routes/index.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import accountRoutes from './accountRoutes.js';
+import troopRoutes from './troopRoutes.js';
+import spellRoutes from './spellRoutes.js';
+import siegeRoutes from './siegeRoutes.js';
+import petRoutes from './petRoutes.js';
+import heroRoutes from './heroRoutes.js';
+import buildingRoutes from './buildingRoutes.js';
+
+const api = Router();
+
+// Mount under /api in server (app.use('/api', api))
+api.use('/accounts', accountRoutes);
+api.use('/troops', troopRoutes);
+api.use('/spells', spellRoutes);
+api.use('/sieges', siegeRoutes);
+api.use('/pets', petRoutes);
+api.use('/heroes', heroRoutes);
+api.use('/buildings', buildingRoutes);
+
+export default api;
