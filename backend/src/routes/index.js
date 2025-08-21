68b7da3f8dc808fda7d9b4b8cf378efdd0bd83b0
ke-kaa <amsalukaku122@gmail.com>
2025-08-21 21:45:17 +0300

    create base routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 import accountRoutes from './accountRoutes.js';
+import authRoutes from './AuthRoutes.js';
 import troopRoutes from './troopRoutes.js';
 import spellRoutes from './spellRoutes.js';
 import siegeRoutes from './siegeRoutes.js';
 import petRoutes from './petRoutes.js';
 import heroRoutes from './heroRoutes.js';
 import buildingRoutes from './buildingRoutes.js';
+import wallRoutes from './wallRoutes.js';
 
 const api = Router();
 
 // Mount under /api in server (app.use('/api', api))
+api.use('/auth', authRoutes);
 api.use('/accounts', accountRoutes);
 api.use('/troops', troopRoutes);
 api.use('/spells', spellRoutes);
@@ -17,5 +20,6 @@ api.use('/sieges', siegeRoutes);
 api.use('/pets', petRoutes);
 api.use('/heroes', heroRoutes);
 api.use('/buildings', buildingRoutes);
+api.use('/walls', wallRoutes);
 
 export default api;
